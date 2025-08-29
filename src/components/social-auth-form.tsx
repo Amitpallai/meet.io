c47956e7d73992b0966fe736f6009cac4b8298b0
Amit6841 <amitkumarpallai907@gmail.com>
2025-08-29 22:14:13 +0530

    Auth

--- src/components/social-auth-form.tsx
+++ src/components/social-auth-form.tsx
@@ -1,24 +1,22 @@
-"use client"
+"use client";
+
 import * as React from "react";
 import { signIn } from "next-auth/react";
-import { Icons } from './ui/icons';
+import { Icons } from "./ui/icons";
 import { useSearchParams, useRouter } from "next/navigation";
 import { Button } from "./ui/button";
 import { useToast } from "./ui/use-toast";
- import { GitHubLogoIcon } from "@radix-ui/react-icons";
 import { GoogleIcon } from "@100mslive/react-icons";
 
 const socialProviders = [
-  { name: "github", color: "#FFF", icon: GitHubLogoIcon },
-  { name: "google", color: "#0f172a", icon:GoogleIcon },
+  { name: "google", color: "#0f172a", icon: GoogleIcon },
 ];
 
-export default function SocialAuthForm () {
+export default function SocialAuthForm() {
   const [isSocialLoading, setIsSocialLoading] = React.useState<{ [key: string]: boolean }>({});
-  const { toast } = useToast()
-  const searchParams = useSearchParams()
-  
-  const router = useRouter()
+  const { toast } = useToast();
+  const searchParams = useSearchParams();
+  const router = useRouter();
 
   const handleSocialSignIn = (provider: string) => {
     setIsSocialLoading((prevLoading) => ({
@@ -26,7 +24,7 @@ export default function SocialAuthForm () {
       [provider]: true,
     }));
 
-    signIn(provider, { 
+    signIn(provider, {
       callbackUrl: searchParams?.get("from") || "/calls",
       redirect: true,
     })
@@ -51,31 +49,31 @@ export default function SocialAuthForm () {
   };
 
   return (
-    <section className='w-full mx-auto flex flex-col gap-2'>
+    <section className="w-full mx-auto flex flex-col gap-2">
       {socialProviders.map((provider) => (
-        <Button 
+        <Button
           key={provider.name}
           size="lg"
           className="font-normal px-1"
-          variant={provider.name === "github" ? "outline" : "outline"}
+          variant="outline"
           onClick={() => handleSocialSignIn(provider.name)}
           disabled={isSocialLoading[provider.name]}
         >
           {isSocialLoading[provider.name] ? (
-            <Icons.spinner 
-              width="16" 
-              height="16" 
+            <Icons.spinner
+              width="16"
+              height="16"
               className="mx-3 px-2"
             />
           ) : (
-            <provider.icon 
-              color={provider.color} 
-              width="16" 
-              height="16" 
+            <provider.icon
+              color={provider.color}
+              width="16"
+              height="16"
               className="mr-3"
             />
           )}
-          Continue with {provider.name === "github" ? "GitHub" : provider.name.charAt(0).toUpperCase() + provider.name.slice(1)}
+          Continue with {provider.name.charAt(0).toUpperCase() + provider.name.slice(1)}
         </Button>
       ))}
     </section>
